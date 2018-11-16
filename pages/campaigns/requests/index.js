3a86c2cb92b4303adaa27e11abeaa7c7ea4db616
ItaRub <itamarr8@gmail.com>
2018-11-16 19:26:45 +0200

    show pending request componenet

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,11 +1,24 @@
-import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
-import { Link } from "../../../routes";
-import Layout from "../../../components/Layout";
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
-    return { address: address };
+    const campaign = Campaign(address);
+    // return the length of the requests
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    //Solidity cant yet return array of struct
+    const requests = await Promise.all(
+      Array(requestCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    return { address, requests, requestCount };
   }
   render() {
     return (
