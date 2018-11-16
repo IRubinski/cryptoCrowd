4b26414ba12713daed7cdbb6d6b117c7fb352ef8
ItaRub <itamarr8@gmail.com>
2018-11-16 21:12:18 +0200

    Create RequestRow Componenet

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
+
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
@@ -11,16 +13,29 @@ class RequestIndex extends Component {
     const requestCount = await campaign.methods.getRequestsCount().call();
     //Solidity cant yet return array of struct
     const requests = await Promise.all(
-      Array(requestCount)
+      Array(parseInt(requestCount))
         .fill()
         .map((element, index) => {
           return campaign.methods.requests(index).call();
         })
     );
-
     return { address, requests, requestCount };
   }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          id={index}
+          request={request}
+          address={this.props.address}
+        />
+      );
+    });
+  }
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
     return (
       <Layout>
         <h3>Index of Requests</h3>
@@ -29,6 +44,20 @@ class RequestIndex extends Component {
             <Button primary>New Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
       </Layout>
     );
   }
